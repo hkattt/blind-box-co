67c13b43ad99193cea1b18b8911fded3bb610f8a
hkattt <hugo.kat3@gmail.com>
2025-06-19 23:52:22 +1000

    Started working on package xray

--- src/scripts/Interrogation.gd
+++ src/scripts/Interrogation.gd
@@ -4,6 +4,7 @@ signal interrogation_complete
 
 @onready var character_slot: Control = $CharacterSlot
 @onready var package_slot: Control   = $PackageSlot
+@onready var xray: Area2D = $CanvasLayer/XRay
 
 var character_scene: PackedScene = preload("res://scenes/Character.tscn")
 var package_scene: PackedScene   = preload("res://scenes/Package.tscn")
@@ -15,6 +16,7 @@ func setup(character_data: CharacterData, package_data: PackageData):
 	
 	var package: Node2D = package_scene.instantiate()
 	package_slot.add_child(package)
+	package.set_xray(xray)
 	package.load_package(package_data)
 
 func _on_approve_button_pressed() -> void:
--- src/scripts/PackageDisplay.gd
+++ src/scripts/PackageDisplay.gd
@@ -1,6 +1,21 @@
 extends Node2D 
 
-@onready var sprite = $Sprite2D 
+@onready var sprite: Sprite2D = $Sprite2D 
+@onready var mask: Sprite2D = $Mask 
 
+var shader: Shader = preload("res://shaders/Package.gdshader")
+var xray: Area2D
+
+func set_xray(_xray: Area2D):
+	xray = _xray 
+	
 func load_package(package_data: PackageData):
 	sprite.texture = package_data.image
+	var shader_material: ShaderMaterial = ShaderMaterial.new() 
+	shader_material.shader = shader
+	sprite.material = shader_material
+
+func _process(_delta: float) -> void:
+	sprite.material.set_shader_parameter("mask_texture", mask.texture)
+	sprite.material.set_shader_parameter("mask_position", mask.global_position / Vector2(get_viewport().size))
+	sprite.material.set_shader_parameter("mask_size", mask.texture.get_size() / Vector2(get_viewport().size))
